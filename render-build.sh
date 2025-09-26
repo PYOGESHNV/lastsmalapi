71f77d910b78f078b205e44273b0c80bfd8b3708
PYOGESHNV <yogesh12579853dzrd@gmail.com>
2025-09-26 07:56:13 +0530

    Initial commit

---
+++ render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Install Maven
+curl -sSL https://apache.osuosl.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz | tar -xz -C /tmp
+export PATH=/tmp/apache-maven-3.9.6/bin:$PATH
+
+# Build Spring Boot project
+mvn clean package -DskipTests
