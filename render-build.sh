46a831a5cde94b5b003972f8ad5ab4215576a940
PYOGESHNV <yogesh12579853dzrd@gmail.com>
2025-09-26 08:24:37 +0530

    urlpdate

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,9 @@
 set -o errexit
 
 # Install Maven
-curl -sSL https://apache.osuosl.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz | tar -xz -C /tmp
+curl -sSL https://downloads.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz | tar -xz -C /tmp
 export PATH=/tmp/apache-maven-3.9.6/bin:$PATH
+
 
 # Build Spring Boot project
 mvn clean package -DskipTests
